afcd6dbab7b6abdc3f678d7b26d908e3fe5bb85f
OKAFORCHINEDU <chineducornelius.okafor@yahoo.com>
2022-07-12 22:33:28 -0700

    Keygen

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main- This generates random valid passwords for the program 101-crackme.
+ *
+ * Returns an integer value of the function
+ *
+ */
+
+int main(void)
+
+{
+
+	int myrand;
+
+	int count;
+	int total;
+
+	srand(time(NULL));
+
+	for (count = 0, total = 2772; total > 122; count++)
+	{
+		myrand = (rand() % 125) + 1;
+		printf("%c", myrand);
+		total -= myrand;
+	}
+	printf("%c", total);
+
+	return (0);
+
+}

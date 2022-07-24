ce10821b28b491efc6941bd3e5a91ef8671a0686
OKAFORCHINEDU <chineducornelius.okafor@yahoo.com>
2022-07-24 08:14:01 -0700

    Mynameis

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+
+/**
+ * main - This a program that prints its name.
+ * @argc: argument counter
+ * @argv: argument vector
+ *
+ * Return: Always 0
+ *
+ */
+
+int main(int argc, char *argv[])
+
+{
+
+	printf("%s\n", argv[0]);
+	(void)argc;
+
+	return (0);
+}

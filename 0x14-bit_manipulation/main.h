f67357b77f42255eb8deeb18ff702aa8dec71370
OKAFORCHINEDU <chineducornelius.okafor@yahoo.com>
2022-08-18 17:02:51 -0700

    Solution to bit manipulation tasks

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -1,5 +1,5 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef _MAIN_H_
+#define _MAIN_H_
 
 
 
@@ -23,4 +23,4 @@ unsigned int flip_bits(unsigned long int n, unsigned long int m);
 int get_endianness(void);
 
 
-#endif /* MAIN_H */
+#endif
